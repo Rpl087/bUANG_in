125db383b39913ea4918545e501373385a938e83
Romal Lengkong <romalputra123@gmail.com>
2023-12-02 10:57:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bUANG_in/SignUp_activity_main.java
@@ -0,0 +1,14 @@
+package com.example.bUANG_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp_activity_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_main);
+    }
+}
---
+++ app/src/main/java/com/example/bUANG_in/login_ActivityMain.java
@@ -0,0 +1,14 @@
+package com.example.bUANG_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_main);
+    }
+}
